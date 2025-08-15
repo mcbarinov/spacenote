07b83c7be8a54b3777c1d558ee61dbf7b09c16e3
mcbarinov <mcbarinov@proton.me>
2025-08-15 13:30:02 +0300

    update

--- backend/src/spacenote/core/app.py
+++ backend/src/spacenote/core/app.py
@@ -5,6 +5,7 @@ from spacenote.core.config import CoreConfig
 from spacenote.core.core import Core
 from spacenote.core.errors import AuthenticationError
 from spacenote.core.session.models import AuthToken
+from spacenote.core.space.models import Space
 
 
 class App:
@@ -21,3 +22,7 @@ class App:
         if not self._core.services.user.verify_password(username, password):
             raise AuthenticationError
         return await self._core.services.session.create_session(username)
+
+    async def create_space(self, auth_token: AuthToken, slug: str, title: str) -> Space:
+        current_user = await self._core.services.session.get_authenticated_user(auth_token)
+        return await self._core.services.space.create_space(slug, title, current_user.id)
--- backend/src/spacenote/core/session/service.py
+++ backend/src/spacenote/core/session/service.py
@@ -4,7 +4,9 @@ from typing import Any
 from pymongo.asynchronous.database import AsyncDatabase
 
 from spacenote.core.core import Service
+from spacenote.core.errors import AuthenticationError
 from spacenote.core.session.models import AuthToken, Session
+from spacenote.core.user.models import User
 
 
 class SessionService(Service):
@@ -13,7 +15,7 @@ class SessionService(Service):
     def __init__(self, database: AsyncDatabase[dict[str, Any]]) -> None:
         super().__init__(database)
         self._collection = database.get_collection("sessions")
-        # self._session_cache: dict[SessionId, User] = {}
+        self._authenticated_users: dict[AuthToken, User] = {}
 
     async def create_session(self, username: str) -> AuthToken:
         user = self.core.services.user.get_user_by_username(username)
@@ -21,3 +23,20 @@ class SessionService(Service):
         new_session = Session(user_id=user.id, auth_token=auth_token)
         await self._collection.insert_one(new_session.to_mongo_dict())
         return auth_token
+
+    async def get_authenticated_user(self, auth_token: AuthToken) -> User:
+        """Get authenticated user from auth_token."""
+
+        # Check cache first
+        if auth_token in self._authenticated_users:
+            return self._authenticated_users[auth_token]
+
+        # Get session from database
+        session = await self._collection.find_one({"auth_token": auth_token})
+        if session is None:
+            raise AuthenticationError("Invalid or expired session")
+
+        user = self.core.services.user.get_user(session["user_id"])
+        self._authenticated_users[auth_token] = user
+
+        return user
